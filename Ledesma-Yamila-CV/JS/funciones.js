4af574b1d8d479e8c2676453144737a9a96cbdb7
yamilaledesma7 <123857108+yamilaledesma7@users.noreply.github.com>
2023-02-28 21:41:04 -0300

    Este es mi primera escritura en el repositorio
    A continuación, se encuentra la versión "final" del proyecto.

---
+++ Ledesma-Yamila-CV/JS/funciones.js
@@ -0,0 +1,53 @@
+function mostrarEsp() {
+    document.getElementById('nat').style.display = 'inline';
+    document.getElementById('btnEspM').style.display = 'none';
+    document.getElementById('btnEspO').style.display = 'inline';
+} 
+
+function ocultarEsp() {
+    document.getElementById('nat').style.display = 'none';
+    document.getElementById('btnEspM').style.display = 'inline';
+    document.getElementById('btnEspO').style.display = 'none';
+} 
+
+function mostrarIng() {
+    document.getElementById('usCom').style.display = 'inline';
+    document.getElementById('btnIngM').style.display = 'none';
+    document.getElementById('btnIngO').style.display = 'inline';
+} 
+
+function ocultarIng() {
+    document.getElementById('usCom').style.display = 'none';
+    document.getElementById('btnIngM').style.display = 'inline';
+    document.getElementById('btnIngO').style.display = 'none';
+} 
+
+function mostrarJap() {
+    document.getElementById('usB1').style.display = 'inline';
+    document.getElementById('btnJapM').style.display = 'none';
+    document.getElementById('btnJapO').style.display = 'inline';
+} 
+
+function ocultarJap() {
+    document.getElementById('usB1').style.display = 'none';
+    document.getElementById('btnJapM').style.display = 'inline';
+    document.getElementById('btnJapO').style.display = 'none';
+} 
+
+function mostrarAl() {
+    document.getElementById('usB2').style.display = 'inline';
+    document.getElementById('btnAlM').style.display = 'none';
+    document.getElementById('btnAlO').style.display = 'inline';
+} 
+
+function ocultarAl() {
+    document.getElementById('usB2').style.display = 'none';
+    document.getElementById('btnAlM').style.display = 'inline';
+    document.getElementById('btnAlO').style.display = 'none';
+} 
+
+function toggleMenu() {
+    menu.classList.toggle("menu");
+  }
+  
+ 
